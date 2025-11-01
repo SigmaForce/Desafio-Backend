431a17bf02d8644df723e75c9679aec29bd17307
Leonardo Matos <leonardolucas2020@gmail.com>
2025-11-01 11:17:19 -0400

    :tada: build: initial config

---
+++ src/server.ts
@@ -0,0 +1,40 @@
+import { fastify } from "fastify";
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from "fastify-type-provider-zod";
+
+import { fastifyCors } from "@fastify/cors";
+import { fastifySwagger } from "@fastify/swagger";
+import ScalarApiReference from "@scalar/fastify-api-reference";
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(fastifyCors, {
+  origin: true,
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+});
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "Movies API",
+      version: "1.0.0",
+    },
+  },
+  transform: jsonSchemaTransform,
+});
+
+app.register(ScalarApiReference, {
+  routePrefix: "/docs",
+});
+
+app.listen({ port: 3333, host: "0.0.0.0" }).then(() => {
+  console.log(`HTTP server running on http://localhost:3333`);
+  console.log(`Docs available at http://localhost:3333/docs`);
+});
